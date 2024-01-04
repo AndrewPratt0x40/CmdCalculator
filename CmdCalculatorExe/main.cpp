f49b3551925780d9a2342cb714666dc9bf39b292
andrewpratt0x40 <andrewpratt0x40@gmail.com>
2024-01-04 15:15:39 -0500

    Improved incremental building for CmdCalculatorAntlrgenerator project.

--- CmdCalculatorExe/main.cpp
+++ CmdCalculatorExe/main.cpp
@@ -2,16 +2,18 @@
 
 #include "../submodules/Antlr4CppRuntime/src/antlr4-runtime.h"
 #include "../CmdCalculatorAntlr/generated_code/CmdCalculatorExpressionLexer.h"
-#include "../CmdCalculatorAntlr/generated_code/CmdCalculatorExpressionListener.h"
+#include "../CmdCalculatorAntlr/generated_code/CmdCalculatorExpressionBaseListener.h"
 #include "../CmdCalculatorAntlr/generated_code/CmdCalculatorExpressionParser.h"
 
 int main()
 {
 	antlr4::ANTLRInputStream inputStream{ " 12 + 2 - 9082 *7/3 " };
-	/*CmdCalculator::Antlr::CmdCalculatorExpressionLexer lexer{&inputStream};
+	CmdCalculator::Antlr::CmdCalculatorExpressionLexer lexer{&inputStream};
 	antlr4::CommonTokenStream tokenStream{ &lexer };
-	CmdCalculator::Antlr::CmdCalculatorExpressionParser parser{ &tokenStream };*/
-	//parser.full_expression();
+	CmdCalculator::Antlr::CmdCalculatorExpressionParser parser{ &tokenStream };
+	auto* fullExpression{ parser.full_expression() };
+	CmdCalculator::Antlr::CmdCalculatorExpressionBaseListener listener{};
+	antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, fullExpression);
 
 	std::cout << "Hello World!\n";
 }
