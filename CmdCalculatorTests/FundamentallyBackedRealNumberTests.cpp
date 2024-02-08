f074db0c41354014f7898339cb46d2dd663fbd66
andrewpratt0x40 <andrewpratt0x40@gmail.com>
2024-02-08 13:18:29 -0500

    wip

--- CmdCalculatorTests/FundamentallyBackedRealNumberTests.cpp
+++ CmdCalculatorTests/FundamentallyBackedRealNumberTests.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <concepts>
 #include <iostream>
+#include <regex>
 
 #include "../CmdCalculator/FundamentallyBackedRealNumber.h"
 #include "../CmdCalculator/RealNumber.h"
@@ -49,6 +50,62 @@ namespace CmdCalculatorTestDoubleTests
 				CmdCalculator::Arithmetic::FundamentallyBackedRealNumber<TypeParam>
 			>
 		);
+	}
+
+#pragma endregion
+
+
+#pragma region getZero
+
+	template<std::floating_point InnerT, InnerT ZeroV>
+	struct FundamentallyBackedRealNumber_getZero_TestData
+	{
+		using InnerType = InnerT;
+		constexpr static inline InnerType zeroValue{ ZeroV };
+
+		friend std::ostream& operator<<(std::ostream& os, const FundamentallyBackedRealNumber_getInnerValue_TestData<InnerType, InnerValueV>& testData)
+		{
+			os << nameOfInnerType<InnerType> << '{' << testData.innerValue << '}';
+			return os;
+		}
+	};
+
+
+	template<class T>
+	class FundamentallyBackedRealNumbergetZeroTests :
+		public testing::Test
+	{};
+
+	using FundamentallyBackedRealNumbergetZero_Types = testing::Types
+#pragma region Types
+	<
+		FundamentallyBackedRealNumber_getZero_TestData<float, 0.0f>,
+		FundamentallyBackedRealNumber_getZero_TestData<double, 0.0>,
+		FundamentallyBackedRealNumber_getZero_TestData<long double, 0.0l>
+	>;
+#pragma endregion
+
+	TYPED_TEST_CASE
+	(
+		FundamentallyBackedRealNumbergetZeroTests,
+		FundamentallyBackedRealNumberWithTParam_Types
+	);
+	
+	TYPED_TEST(FundamentallyBackedRealNumbergetZeroTests, getZero$returns$zero)
+	{
+		// Arrange
+		using InnerType = typename TypeParam::InnerType;
+		const InnerType expected{ TypeParam::zeroValue };
+
+		// Act
+		const InnerType actual
+		{
+			CmdCalculator::Arithmetic::FundamentallyBackedRealNumber<InnerType>::getZero()
+			.getInnerValue()
+		};
+
+		// Assert
+		EXPECT_EQ(expected, actual);
 	}
 
 #pragma endregion
@@ -120,15 +177,104 @@ namespace CmdCalculatorTestDoubleTests
 		const CmdCalculator::Arithmetic::FundamentallyBackedRealNumber<InnerType> instance
 		{
 			innerValue
-		};
-
-		auto foo = instance == instance;
-
+		};
 		// Act
 		const InnerType actual{ instance.getInnerValue() };
 
 		// Assert
 		EXPECT_EQ(expected, actual);
+	}
+
+#pragma endregion
+
+
+#pragma region getStringRepresentation
+
+	struct FundamentallyBackedRealNumber_getStringRepresentation_TestData
+	{
+		double innerValue;
+		std::string expectedStringRepresentationRegex;
+
+		friend std::ostream& operator<<(std::ostream& os, const FundamentallyBackedRealNumber_getStringRepresentation_TestData& testData)
+		{
+			os
+				<< "FundamentallyBackedRealNumber{"
+				<< testData.innerValue
+				<< "}.getStringRepresentation() matches \""
+				<< testData.expectedStringRepresentationRegex
+				<< '\"'
+			;
+			return os;
+		}
+	};
+	
+	
+	class FundamentallyBackedRealNumbergetStringRepresentationTests :
+		public testing::TestWithParam<FundamentallyBackedRealNumber_getStringRepresentation_TestData>
+	{};
+
+
+	const FundamentallyBackedRealNumber_getStringRepresentation_TestData FundamentallyBackedRealNumber_getStringRepresentation_TestDataValues[]
+	{
+		{
+			.innerValue{ -56.78 },
+			.expectedStringRepresentationRegex{ "\\s*-\\s*0*56\\.780*\\s*" }
+		},
+		{
+			.innerValue{ -12.34 },
+			.expectedStringRepresentationRegex{ "\\s*-\\s*0*12\\.340*\\s*" }
+		},
+		{
+			.innerValue{ -1.0 },
+			.expectedStringRepresentationRegex{ "\\s*-\\s*0*1(\\.0*)?\\s*" }
+		},
+		{
+			.innerValue{ -0.001 },
+			.expectedStringRepresentationRegex{ "\\s*-\\s*0+\\.0010*\\s*" }
+		},
+		{
+			.innerValue{ 0 },
+			.expectedStringRepresentationRegex{ "\\s*(\\+|-)?\\s*0+(\\.0*)?\\s*" }
+		},
+		{
+			.innerValue{ -0.001 },
+			.expectedStringRepresentationRegex{ "\\s*\\+?\\s*0+\\.0010*\\s*" }
+		},
+		{
+			.innerValue{ -1.0 },
+			.expectedStringRepresentationRegex{ "\\s*\\+?\\s*0*1(\\.0*)?\\s*" }
+		},
+		{
+			.innerValue{ -12.34 },
+			.expectedStringRepresentationRegex{ "\\s*\\+?\\s*0*12\\.340*\\s*" }
+		},
+		{
+			.innerValue{ -56.78 },
+			.expectedStringRepresentationRegex{ "\\s*\\+?\\s*0*56\\.780*\\s*" }
+		}
+	};
+
+	INSTANTIATE_TEST_CASE_P
+	(
+		FundamentallyBackedRealNumberTests,
+		FundamentallyBackedRealNumbergetStringRepresentationTests,
+		testing::ValuesIn(FundamentallyBackedRealNumber_getStringRepresentation_TestDataValues)
+	);
+	
+	TEST_P(FundamentallyBackedRealNumbergetStringRepresentationTests, getStringRepresentation$returns$expected$value)
+	{
+		// Arrange
+		const double innerValue{ GetParam().innerValue};
+		const std::regex expectedRegex{ GetParam().expectedStringRepresentationRegex };
+		const CmdCalculator::Arithmetic::FundamentallyBackedRealNumber<double> instance
+		{
+			innerValue
+		};
+		// Act
+		const std::string actual{ instance.getStringRepresentation() };
+
+		// Assert
+		EXPECT_TRUE(std::regex_match(actual, expectedRegex));
 	}
 
 #pragma endregion
